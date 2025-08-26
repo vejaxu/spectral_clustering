f5d8dee617197041b12ea8cf8865544d1f4ed2d7
vejaxu <wjxu29@gmail.com>
2025-08-26 22:59:40 +0800

    更新outliers

--- dataset_construction/outliers.py
+++ dataset_construction/outliers.py
@@ -9,4 +9,4 @@ outliers = 20 + rng.uniform(-1, 1, size=(10, 2))
 data = np.vstack([data_core, outliers])
 labels = np.concatenate([np.ones(1190, dtype=int), np.full(10, 2, dtype=int)])
 
-savemat("/home/xwj/aaa/clustering/data/kmeans/dataset_outliers2.mat", {"data": data, "class": labels})
+savemat("/home/xwj/aaa/clustering/data/kmeans/dataset_outliers.mat", {"data": data, "class": labels})
