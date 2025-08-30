cb41c524c4f882a0f4a035d72582cd03b8c17ba2
vejaxu <wjxu29@gmail.com>
2025-08-30 14:57:45 +0800

    imbalanced

--- kmeans/dataset_construction/imbalanced.py
+++ kmeans/dataset_construction/imbalanced.py
@@ -1,12 +1,28 @@
 import numpy as np
 from scipy.io import savemat
 
+# 设置随机种子
 rng = np.random.default_rng(42)
 
-data2_a = rng.multivariate_normal([0, 0], np.eye(2), 1100)
-data2_b = rng.multivariate_normal([5, 0], np.eye(2), 100)
+# ----------------------------
+# 生成三类球状数据
+# ----------------------------
 
-data = np.vstack([data2_a, data2_b])
-labels = np.concatenate([np.ones(1100, dtype=int), np.full(100, 2, dtype=int)])
+# 类1：大球，中心在原点，点数多，方差适中（球状扩散）
+data1 = rng.multivariate_normal([0, 0], 1.0 * np.eye(2), 1000)
+
+# 类2：小球，中心在 [4, 0]，方差小（紧凑）
+data2 = rng.multivariate_normal([4, 0], 0.2 * np.eye(2), 200)
+
+# 类3：小球，中心在 [5, 0]，与类2 靠近但分离
+data3 = rng.multivariate_normal([5, 0], 0.2 * np.eye(2), 200)
+
+# 合并数据和标签
+data = np.vstack([data1, data2, data3])
+labels = np.concatenate([
+    np.ones(1000, dtype=int),
+    np.full(200, 2, dtype=int),
+    np.full(200, 3, dtype=int)
+])
 
 savemat("/home/xwj/aaa/clustering/data/kmeans/dataset_imbalanced.mat", {"data": data, "class": labels})
